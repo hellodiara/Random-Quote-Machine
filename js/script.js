5336f4dd279a731c1ba50cc09ed0b8c406104b87
Diara-Bamba <diarabamba@gmail.com>
2018-08-07 16:53:44 -0400

    Display quote

--- js/script.js
+++ js/script.js
@@ -4,16 +4,22 @@ newQuoteButton.addEventListener('click', getQuote);
 
 const endPoint = 'https://talaikis.com/api/quotes/random/';
 
+// Retrieves quote from API
 function getQuote() {
 	fetch(endPoint)
 	    .then(function (response) {
 	      return response.json();
 	    })
-	    .then(function (data) {
-	      console.log(data);
+	    .then(function (quote) {
+	      displayQuote(quote);
 	    })
 	    .catch(function () {
 	      console.log("An error occurred");
 	    });
 }
 
+// Displays the quote on the page
+function displayQuote(quote) {
+	const quoteText = document.querySelector('.quote-text');
+	quoteText.textContent =quote;
+}

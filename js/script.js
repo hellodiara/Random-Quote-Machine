3656e391e769bc6a7126d909be776cf358723b36
Diara-Bamba <diarabamba@gmail.com>
2018-08-07 16:21:42 -0400

    Initial files

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const newQuoteButton= document.querySelector('.new-quote');
+newQuoteButton.addEventListener('click', getQuote);
+
+function getQuote() {
+	console.log('newQuoteButton button was clicked');
+}
